b999e01e14bb2708cf83d1ca5b1b5cdad8446696
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-07 15:22:22 +0100

    :sparkles: Create seeds for ingredients

--- data/seeds/ingredient_seeds.js
+++ data/seeds/ingredient_seeds.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('ingredients').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('ingredients').insert([
+        {ingredient_name: 'water', description: "Required for everything, plus, wash your hands"},
+        {ingredient_name: 'water', description: "Required for everything, plus, wash your hands"},
+        {ingredient_name: 'water', description: "Required for everything, plus, wash your hands"}
       ]);
     });
 };
