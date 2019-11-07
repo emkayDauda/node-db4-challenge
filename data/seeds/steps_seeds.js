41b91ab0586b4687c63aaba526272365aac7827b
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-07 17:49:24 +0100

    :sparkles: Add moar SEEDS

--- data/seeds/steps_seeds.js
+++ data/seeds/steps_seeds.js
@@ -6,6 +6,8 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('steps').insert([
         {recipe_id: 1, instruction: "Wash your hands, you pig!", step_number: 1},
+        {recipe_id: 1, instruction: "Wash the rice, you pig!", step_number: 2},
+        {recipe_id: 1, instruction: "Boil the water, you slob!", step_number: 3},
         {recipe_id: 2, instruction: "Wash your hands, you pig!", step_number: 1},
         {recipe_id: 3, instruction: "Wash your hands, you pig!", step_number: 1}
       ]);
